4f174281fa469762be7820e9d1739448dc426184
Nikita <jrringler13759@gmail.com>
2020-05-14 16:43:11 -0400

    it functions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # HW10
 HW 10 Team Page Generator
+run node app.js and follow the on screen prompts in your console
+When you have finished entering employees, select "I do not want to enter another employee" 
+You html page will automatically be made in the output folder.
--- app.js
+++ app.js
@@ -7,57 +7,66 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-//what are these? Are we not using util to promisify?
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 
 const render = require("./lib/htmlRenderer");
 
+const employees = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-    //ask the manager questions
-    //ask if they want to add an employee
-    //4th question depends on answer from 3rd question
-
-    //employee to manager or just manager and then Engineer and Intern
-
+//Prompt manager for their information
 function promptManager() {
     return inquirer.prompt ([
         { 
             type: "input",
             name: "name",
-            message: "What is your manager's name?"  
+            message: "What is your manager's name?",
+            validate: answer => {
+                if (answer !== ""){
+                    return true;
+                } else {
+                    return "You must enter a name"
+                }
+
+            }   
         },
 
         { 
             type: "input",
             name: "id",
             message: "What is your manager's id number?",
-            validate: needANumber(userInput.id)
+            // validate: answer => {
+            //     if (typeOf(answer) !== "number"){
+            //         return "You must enter a number.";
+            //     }
+            // }
+            
         },
 
         { 
             type: "input",
             name: "email",
             message: "What is your manager's email?",
-            validate: ""
+            // validate: function(answer){
+            //     validator.validate(answer);
+            // }
         },
 
         { 
             type: "input",
             name: "office",
             message: "What is the manager's office number?",
-            when: ""
         }
     ])
 }
 
 promptManager()
 .then(function(answers){
-//how do I get the employee questions to link to the 4th appropriate question?
+    const manager = new Manager(answers.name, answers.id, answers.email, answers.office);
+    employees.push(manager);
     chooseNext();
 })
 .catch(function(err){
@@ -65,61 +74,142 @@ promptManager()
 })
 
 
-function chooseNext () {
+//If engineer is chosen these questions will be asked next
+function promptEngineer(){
     return inquirer.prompt ([
-        {
-            type: "list",
-            name: "next",
-            message: "Which type of employee would you like to add?",
-            choices: ["Engineer", "Intern", "I don't want to enter another employee"]
+        { 
+            type: "input",
+            name: "name",
+            message: "What is your engineers's name?",
+            // validate: function(answer){
+            //     if (answer == ""){
+            //         return "You must enter a name.";
+            //     } 
+            // }   
+        },
+
+        { 
+            type: "input",
+            name: "id",
+            message: "What is your engineer's id number?",
+            // validate: function(answer){
+            //     if (typeOf(answer) !== "number"){
+            //         return "You must enter a number.";
+            //     }
+            // } 
+        },
+
+        { 
+            type: "input",
+            name: "email",
+            message: "What is your engineer's email?",
+            // validate: function(answer){
+            //     validator.validate(answer);
+            // }
         },
 
         { 
             type: "input",
             name: "github",
-            message: "What is your engineer's GitHub username?" 
-            when: (userInput) => {if(userInput.next === "Engineer"){
-                
-            }} 
-        },
-
-        { 
-            type: "input",
-            name: "school",
-            message: "What is your intern's school?"  
-        },
-
-        
+            message: "What is the engineer's github username?",
+        }
     ])
+
+    .then(function(answers){
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        employees.push(engineer);
+        chooseNext();
+    })
+    .catch(function(err){
+        console.log(err);
+    })
 }
 
 
 
 
+//If the intern is chosen these questions will be asked next
+function promptIntern(){
+    return inquirer.prompt ([
+        { 
+            type: "input",
+            name: "name",
+            message: "What is your intern's name?",
+            // validate: function(answer){
+            //     if (answer == ""){
+            //         return "You must enter a name.";
+            //     } 
+            // }   
+        },
+
+        { 
+            type: "input",
+            name: "id",
+            message: "What is your intern's id number?",
+            // validate: function(answer){
+            //     if (typeOf(answer) !== "number"){
+            //         return "You must enter a number.";
+            //     }
+            // }
+            
+        },
+
+        { 
+            type: "input",
+            name: "email",
+            message: "What is your intern's email?",
+            // validate: function(answer){
+            //     validator.validate(answer);
+            // }
+        },
+
+        { 
+            type: "input",
+            name: "school",
+            message: "What is the intern's school?",
+        }
+    ])
+
+    .then(function(answers){
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        employees.push(intern);
+        chooseNext();
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+}
 
 
+//Determine the next employee if they want it
+function chooseNext () {
+    inquirer.prompt ([
+        {
+            type: "list",
+            name: "next",
+            message: "Which type of employee would you like to add?",
+            choices: ["Engineer", "Intern", "I don't want to enter another employee"]
+        }
+    ])
+    .then(function(ans){
+       
+        switch (ans.next) {
+            case "Engineer":
+                promptEngineer();
+                break;
+            case "Intern":
+                promptIntern();
+                break;
+            default:
+                const renderHtml = render(employees);
+                fs.writeFile(outputPath, renderHtml, function(err){
+                    if(err){
+                        console.log(err);
+                    } else {
+                        console.log("The file has been written");
+                    }
+                })
+        }
+    })
+}
 
-
-
-
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
